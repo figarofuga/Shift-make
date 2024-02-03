34f5da2380f06fae6e7641d40827895b51b3611a
figarofuga <61215157+figarofuga@users.noreply.github.com>
2024-02-03 03:41:23 +0000

    update data and pyfile

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Shift-make
+
+The aim of this project is to create a simple and easy to use tool for creating and managing shift schedules. The tool should be able to handle multiple shifts, multiple employees and multiple locations. The tool should also be able to handle different types of shifts, such as day shifts, night shifts, and on-call shifts. 
+Furthermore, this is my personal practice of python, polars and etc.
+This project is based on miniconda3. 
--- shiftmake.py
+++ shiftmake.py
@@ -1,8 +1,26 @@
 #%%
+import numpy as np
 import pandas as pd
 import polars as pl
 import seaborn as sns
 import pulp
+from datetime import date
 
 #%%
+# read csv data
+dat=pl.read_csv("testshiftdata.csv")
+
+#%%
+# make test data
+# to make columns of A to Z that is random choice of "no", "soso", "good"
+
+dat2= pl.DataFrame({
+    "date": pl.date_range(
+    date(2024, 5, 1), 
+    date(2024, 5, 31), 
+    "1d", 
+    eager=True),
+    **{chr(65+i): np.random.choice(["no", "soso", "good"], replace=True, p=[0.5, 0.3, 0.2], size=31) for i in range(26)}
+})
+
 
