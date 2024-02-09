88efbb17dbdf0ca54daa0558b728f8d790d9dc11
figarofuga <61215157+figarofuga@users.noreply.github.com>
2024-02-09 13:19:25 +0000

    columns type conversion

--- shiftmake.py
+++ shiftmake.py
@@ -16,16 +16,21 @@ dat=pl.read_csv("testshiftdata.csv")
 # make test data
 # to make columns of A to Z that is random choice of "no", "soso", "good"
 
-dat2= pl.DataFrame({
-    "date": pl.date_range(
-    date(2024, 5, 1), 
-    date(2024, 5, 31), 
-    "1d", 
-    eager=True),
-    **{chr(65+i): np.random.choice(["no", "soso", "good"], replace=True, p=[0.5, 0.3, 0.2], size=31) for i in range(26)}
-})
+# dat2= pl.DataFrame({
+#     "date": pl.date_range(
+#     date(2024, 5, 1), 
+#     date(2024, 5, 31), 
+#     "1d", 
+#     eager=True),
+#     **{chr(65+i): np.random.choice(["no", "soso", "good"], replace=True, p=[0.5, 0.3, 0.2], size=31) for i in range(26)}
+# })
 
+# convert "date" column to date type column 
 
-
+dat2=(dat.with_columns(
+    [pl.col("date")
+        .str.strptime(pl.Date,
+                      strict=False)])
+)
 
 # %%
