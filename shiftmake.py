2184131fa6ccc0795ebc0e0c19de5ae6af739d23
figarofuga <61215157+figarofuga@users.noreply.github.com>
2024-02-14 14:18:03 +0000

    slice columns

--- shiftmake.py
+++ shiftmake.py
@@ -13,6 +13,15 @@ import datetime
 dat=pl.read_excel("2024_03data.xlsx")
 
 #%%
+
+start_column='タイムスタンプ'
+stop_column='日直・当直希望についての備考'
+all_columns=dat.columns
+start_index=all_columns.index(start_column)
+stop_index=all_columns.index(stop_column)
+
+dat2=dat.select(all_columns[start_index:stop_index])
+
 # make test data
 # to make columns of A to Z that is random choice of "no", "soso", "good"
 
@@ -26,20 +35,20 @@ dat=pl.read_excel("2024_03data.xlsx")
 # })
 
 # convert "date" column to date type column 
-def is_holiday(date):
-    return jpholiday.is_holiday(date)
+# #  def is_holiday(date):
+# #     return jpholiday.is_holiday(date)
 
-# Define a function that checks if a date is a holiday
+# # Define a function that checks if a date is a holiday
 
-dat2=(dat.with_columns(
-    [pl.col("date")
-        .str.strptime(pl.Date,
-                      strict=False)
-# make date column to date type to concordant with jpholiday package
-        .apply(lambda x: datetime.date(x.year, x.month, x.day))
-        .apply(is_holiday).alias("holiday")])
+# dat2=(dat.with_columns(
+#     [pl.col("date")
+#         .str.strptime(pl.Date,
+#                       strict=False)
+# # make date column to date type to concordant with jpholiday package
+#         .apply(lambda x: datetime.date(x.year, x.month, x.day))
+#         .apply(is_holiday).alias("holiday")])
                   
-)
+# )
 
 
 
