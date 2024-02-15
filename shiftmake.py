15844a97617ab93b4e210e4672bc4a69ac6bc730
figarofuga <61215157+figarofuga@users.noreply.github.com>
2024-02-15 12:58:46 +0000

    rename columns

--- shiftmake.py
+++ shiftmake.py
@@ -7,6 +7,7 @@ import pulp
 from datetime import date
 import jpholiday
 import datetime
+import re
 
 #%%
 # read csv data
@@ -17,39 +18,18 @@ dat=pl.read_excel("2024_03data.xlsx")
 start_column='タイムスタンプ'
 stop_column='日直・当直希望についての備考'
 all_columns=dat.columns
+
 start_index=all_columns.index(start_column)
 stop_index=all_columns.index(stop_column)
 
-dat2=dat.select(all_columns[start_index:stop_index])
 
-# make test data
-# to make columns of A to Z that is random choice of "no", "soso", "good"
+dat2 = (dat
+        .select(all_columns[(start_index+4):stop_index])
+        .rename(lambda c: re.search(r"\d{1,2}(.*)\)", c).group())
+)
 
-# dat2= pl.DataFrame({
-#     "date": pl.date_range(
-#     date(2024, 5, 1), 
-#     date(2024, 5, 31), 
-#     "1d", 
-#     eager=True),
-#     **{chr(65+i): np.random.choice(["no", "soso", "good"], replace=True, p=[0.5, 0.3, 0.2], size=31) for i in range(26)}
-# })
-
-# convert "date" column to date type column 
-# #  def is_holiday(date):
-# #     return jpholiday.is_holiday(date)
-
-# # Define a function that checks if a date is a holiday
-
-# dat2=(dat.with_columns(
-#     [pl.col("date")
-#         .str.strptime(pl.Date,
-#                       strict=False)
-# # make date column to date type to concordant with jpholiday package
-#         .apply(lambda x: datetime.date(x.year, x.month, x.day))
-#         .apply(is_holiday).alias("holiday")])
-                  
-# )
-
+dat3 = pl.concat([dat.select(all_columns[start_index:start_index+4]), 
+                 dat2], how="horizontal")
 
 
 # `holiday` column will be of type Boolean: True if the date is a holiday, False otherwise
