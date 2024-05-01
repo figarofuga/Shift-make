20909d4f29ce753026542070e40acbd776246a21
figarofuga <61215157+figarofuga@users.noreply.github.com>
2024-05-01 09:38:40 +0000

    new data

--- result_prep.py
+++ result_prep.py
@@ -1,40 +1,13 @@
 #%%
+import numpy as np
 import pandas as pd
 import pickle
 import re
 
 #%%
-# read excel data
-res_dat_5 = pd.read_excel("rawdata/2024_5_res.xlsx")
-res_dat_6 = pd.read_excel("rawdata/2024_6_res.xlsx")
 
-# read pickle of desired data
-with open('long_dict_6.pkl', 'rb') as handle:
-    loaded_data = pickle.load(handle)
-    
-
-#%%
-prep_dat_5 = (res_dat_5
-            .melt(id_vars = "2024年5月当直表", value_name='name', var_name='tochoku')
-            .dropna()
-            .query("tochoku.isin(['D日直', 'E当直', 'F当直', 'A当直', 'B当直', '教育当直', '病棟当直'])")
-            .groupby(["name", "tochoku"])["2024年5月当直表"]
-            .apply(lambda x: ' ,'.join(x))
-            .reset_index()
-            .assign(name = lambda x: x['name'].str.replace('[　 ]', '', regex=True))
-)
-prep_dat_6 = (res_dat_6
-                     .melt(id_vars = "2024年6月当直表", value_name='name', var_name='tochoku')
-                     .dropna()
-                     .query("tochoku.isin(['D日直', 'E当直', 'F当直', 'A当直', 'B当直', '教育当直', '病棟当直'])")
-                     .groupby(["name", "tochoku"])["2024年6月当直表"]
-                     .apply(lambda x: ' ,'.join(x))
-                     .reset_index()
-                     .assign(name = lambda x: x['name'].str.replace('[　 ]', '', regex=True))
-)
-# %%
-
-{'久冨木原': '久冨木原健二', 
+named_list = {
+ '久冨木原': '久冨木原健二', 
  '伊藤(国)': '伊藤国秋', 
  '佐久川': '佐久川桂怜', 
  '佐久間': '佐久間一也', 
@@ -61,19 +34,79 @@ prep_dat_6 = (res_dat_6
  '松原': '松原龍輔', 
  '松永': '松永崇宏', 
  '林(智)': '林智史', 
- '津山', 
- '深沢', 
- '熊木',
- '石井', '福井', '福原', '篠崎', '細川', '織部', '脇坂', '臼坂', '茅島', '荒金', '莇',
-       '藤澤', '谷岡', '野上', '鈴木(徹)', '門松', '雪野', '青木', '高木', '高梨', '黒崎'}
+ '津山': '津山頌章', 
+ '福原': '消化器内科福原誠一郎',
+ '深沢': '深沢夏海', 
+ '熊木': '熊木聡美',
+ '石井': '石井真央', 
+ '福井': '福井梓穂', 
+ '篠崎': '篠﨑太郎', 
+ '細川': '細川善弘', 
+ '織部': '織部峻太郎', 
+ '脇坂': '脇坂悠介', 
+ '臼坂': '臼坂優希', 
+ '茅島': '茅島敦人', 
+ '荒金': '荒金直美', 
+ '莇': '莇舜平',
+ '藤澤': '藤澤まり', 
+ '谷岡': '谷岡友則', 
+ '野上': '野上創生', 
+ '鈴木(徹)': '鈴木徹志朗', 
+ '門松': '腎内門松', 
+ '雪野': '雪野満', 
+ '青木': '青木康浩', 
+ '高木': '髙木菜々美', 
+ '高梨': '髙梨航輔', 
+ '黒崎': '黒崎颯' }
 
- , , ,
-       '津山\u3000頌章', '消化器内科\u3000\u3000福原\u3000誠一郎',
-       '消化器内科\u3000渡邉\u3000多代', '清水\u3000隆之', '熊木聡美', '片山充哉',
-       '石井\u3000真央', '福井梓穂', '福島龍貴', '篠﨑太郎', '籠尾壽哉', '糖尿病科岩瀬', '細川善弘',
-       '織部\u3000峻太郎', '腎内\u3000門松', '腎臓内科\u3000松浦', '臼坂優希', '荒金直美', '莇舜平',
-       '藤村 慶子', '藤澤まり', '谷岡友則', '里見\u3000良輔', '野上\u3000創生', '鈴木勝也',
-       '長谷川華子', '雪野\u3000満', '髙木菜々美']
 
+#%%
+# read excel data
+res_dat_5 = pd.read_excel("rawdata/2024_5_res.xlsx")
+res_dat_6 = pd.read_excel("rawdata/2024_6_res.xlsx")
+
+# read pickle of desired data
+with open('long_dict_6.pkl', 'rb') as handle:
+    request_data_6 = pickle.load(handle)
+    
+
+#%%
+prep_dat_5 = (res_dat_5
+            .melt(id_vars = "2024年5月当直表", value_name='name', var_name='tochoku')
+            .dropna()
+            .query("tochoku.isin(['D日直', 'E当直', 'F当直', 'A当直', 'B当直', '教育当直', '病棟当直'])")
+            .groupby(["name", "tochoku"])["2024年5月当直表"]
+            .apply(lambda x: ' ,'.join(x))
+            .reset_index()
+            .assign(name = lambda x: x['name'].str.replace('[　 ]', '', regex=True))
+)
+prep_dat_6 = (res_dat_6
+                     .melt(id_vars = "2024年6月当直表", value_name='name', var_name='tochoku')
+                     .dropna()
+                     .query("tochoku.isin(['D日直', 'E当直', 'F当直', 'A当直', 'B当直', '教育当直', '病棟当直'])")
+                     .groupby(["name", "tochoku"])["2024年6月当直表"]
+                     .apply(lambda x: ' ,'.join(x))
+                     .reset_index()
+                     .assign(name = lambda x: x['name'].str.replace('[　 ]', '', regex=True))
+)
+# %%
+tochoku_request = (request_data_6['tochoku']
+                   .assign(request2 = lambda x: np.select(
+    [
+        x['request'].isin(['○１', '○', '◯', '希望日']),
+        x['request'].isin(['×', '✕', '不可日'])
+    ], 
+    [
+        1, 
+        -1, 
+
+    ],
+    default = 0  # 上記のいずれにも該当しない場合
+))
+
+)
+tmp = prep_dat_6.assign(name = lambda x:x['name'].replace(named_list))
+# %%
+pd.merge(tochoku_request, tmp, on='name', how='outer')
 
 # %%
