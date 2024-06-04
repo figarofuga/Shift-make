18dc44d3b8b5392f3a64a9876e95f330e3ba7724
figarofuga <61215157+figarofuga@users.noreply.github.com>
2024-06-04 13:26:46 +0000

    reconstruct

--- pandas_prepare.py
+++ pandas_prepare.py
@@ -96,9 +96,11 @@ data_concat_tochoku = (pd.concat([base_data, data_tochoku], axis=1)
 
 notes_data_tochoku = (dat_notes
                 .filter(regex=r'人|日付|日直・当直.*')
-                .rename(columns={"人": "name", 
-                                 "日付": "date",
-                                 "日直・当直": "request"}))
+                .rename(columns=lambda x: 'name' if '人' in x else x)
+                .rename(columns=lambda x: 'date' if '日付' in x else x)
+                .rename(columns=lambda x: 'request' if '日直・当直希望' and not '備考' in x else x)
+)
+
 combined_data_tochoku = (pd.concat([data_concat_tochoku, notes_data_tochoku], axis=0)
                             .applymap(lambda x: x.strip() if isinstance(x, str) else x)   
                             .assign(request=lambda x: np.where(x['request'] == '', None, x['request']))   
@@ -131,6 +133,128 @@ comment_tochoku = comment_dat.filter(regex=r'name|日直・当直')
 
 data_wide_tochoku_comment = (data_wide_tochoku_pre
                  .merge(comment_tochoku, on='name', how='left')
+            )
+
+#%%
+# ICU勤務のデータを整形
+data_icu = (dat
+        .filter(regex=r'ICU勤務.*\d{1,2}月')
+        .rename(columns=extract_date)
+)
+data_concat_icu = (pd.concat([base_data, data_icu], axis=1)
+                       .assign(タイムスタンプ=lambda x: pd.to_datetime(x['タイムスタンプ']))
+                          .sort_values(by='タイムスタンプ', ascending=True)
+                            .groupby('お名前')
+                            .last()
+                            .drop(['タイムスタンプ'], axis=1)
+                            .reset_index()
+                            .rename(columns={'お名前': 'name'})
+                            .melt(id_vars=['name'], 
+                                  var_name='date', 
+                                  value_name='request', 
+                                  ignore_index=False)
+)
+# notes dataと結合
+
+notes_data_icu = (dat_notes
+                .filter(regex=r'人|日付|ICU.*')
+                .rename(columns=lambda x: 'name' if '人' in x else x)
+                .rename(columns=lambda x: 'date' if '日付' in x else x)
+                .rename(columns=lambda x: 'request' if 'ICU' and not '備考' in x else x)
+)
+
+combined_data_icu = (pd.concat([data_concat_icu, notes_data_icu], axis=0)
+                            .applymap(lambda x: x.strip() if isinstance(x, str) else x)   
+                            .assign(request=lambda x: np.where(x['request'] == '', None, x['request']))   
+                            .assign(name = lambda x: x['name'].str.replace('[　 ]', '', regex=True))
+                            )
+
+data_wide_icu_pre = (combined_data_icu
+             .groupby(['name', 'request'])['date']
+             .apply(lambda x: ' ,'.join(x))
+             .reset_index()
+             .pivot(index='name', columns='request', values='date')    
+    )
+columns_ok = data_wide_icu_pre.filter(items = ['○', '◯', '希望日']).columns
+if not columns_ok.empty:
+    # 該当する列のデータをコンマ区切りで結合
+    data_wide_icu_pre['accept'] = data_wide_icu_pre[columns_ok].apply(lambda x: ', '.join(x.dropna().astype(str)), axis=1)
+    
+columns_bad = data_wide_icu_pre.filter(items=['×', '✕', '不可日']).columns
+if not columns_bad.empty:
+    # 該当する列のデータをコンマ区切りで結合
+    data_wide_icu_pre['reject'] = data_wide_icu_pre[columns_bad].apply(lambda x: ', '.join(x.dropna().astype(str)), axis=1)
+
+data_wide_icu = (data_wide_icu_pre
+                 .drop(columns=columns_ok)
+                 .drop(columns=columns_bad)
+                 .assign(species = 'icu')
+    )
+
+comment_icu = comment_dat.filter(regex=r'name|ICU勤務')
+
+data_wide_icu_comment = (data_wide_icu_pre
+                 .merge(comment_icu, on='name', how='left')
+            )
+#%%
+# 1次救急のデータを整形
+data_ichijikyu = (dat
+        .filter(regex=r'1次救急.*\d{1,2}月')
+        .rename(columns=extract_date)
+)
+data_concat_ichijikyu = (pd.concat([base_data, data_ichijikyu], axis=1)
+                       .assign(タイムスタンプ=lambda x: pd.to_datetime(x['タイムスタンプ']))
+                          .sort_values(by='タイムスタンプ', ascending=True)
+                            .groupby('お名前')
+                            .last()
+                            .drop(['タイムスタンプ'], axis=1)
+                            .reset_index()
+                            .rename(columns={'お名前': 'name'})
+                            .melt(id_vars=['name'], 
+                                  var_name='date', 
+                                  value_name='request', 
+                                  ignore_index=False)
+)
+# notes dataと結合
+
+notes_data_ichijikyu = (dat_notes
+                .filter(regex=r'人|日付|1次救急.*')
+                .rename(columns=lambda x: 'name' if '人' in x else x)
+                .rename(columns=lambda x: 'date' if '日付' in x else x)
+                .rename(columns=lambda x: 'request' if '1次救急' and not '備考' in x else x)
+)
+combined_data_ichijikyu = (pd.concat([data_concat_ichijikyu, notes_data_ichijikyu], axis=0)
+                            .applymap(lambda x: x.strip() if isinstance(x, str) else x)   
+                            .assign(request=lambda x: np.where(x['request'] == '', None, x['request']))   
+                            .assign(name = lambda x: x['name'].str.replace('[　 ]', '', regex=True))
+                            )
+
+data_wide_ichijikyu_pre = (combined_data_ichijikyu
+             .groupby(['name', 'request'])['date']
+             .apply(lambda x: ' ,'.join(x))
+             .reset_index()
+             .pivot(index='name', columns='request', values='date')    
+    )
+columns_ok = data_wide_ichijikyu_pre.filter(items = ['○', '◯', '希望日']).columns
+if not columns_ok.empty:
+    # 該当する列のデータをコンマ区切りで結合
+    data_wide_ichijikyu_pre['accept'] = data_wide_ichijikyu_pre[columns_ok].apply(lambda x: ', '.join(x.dropna().astype(str)), axis=1)
+    
+columns_bad = data_wide_ichijikyu_pre.filter(items=['×', '✕', '不可日']).columns
+if not columns_bad.empty:
+    # 該当する列のデータをコンマ区切りで結合
+    data_wide_ichijikyu_pre['reject'] = data_wide_ichijikyu_pre[columns_bad].apply(lambda x: ', '.join(x.dropna().astype(str)), axis=1)
+
+data_wide_ichijikyu = (data_wide_ichijikyu_pre
+                 .drop(columns=columns_ok)
+                 .drop(columns=columns_bad)
+                 .assign(species = 'ichijikyu')
+    )
+
+comment_ichijikyu = comment_dat.filter(regex=r'name|1次救急')
+
+data_wide_ichijikyu_comment = (data_wide_ichijikyu_pre
+                 .merge(comment_ichijikyu, on='name', how='left')
             )
 
 #%%
