355d130a46aa3f92962fc3b77c23dedb3eff9219
figarofuga <61215157+figarofuga@users.noreply.github.com>
2024-06-17 11:02:39 +0000

    progression

--- mihon_makeshift.py
+++ mihon_makeshift.py
@@ -51,58 +51,64 @@ availability_dict = create_availability_dict(dat_tochoku)
 
 
 #%%
-employees=(dat_tochoku
- .filter(items=['new_name'])
+all_members = (dat_tochoku
+ .filter(items=['name'])
  .drop_duplicates()
  .values.flatten().tolist()
 )
 
 staffs = (dat_tochoku
           .query('position == "スタッフ"')
-          .filter(items=['new_name'])
+          .filter(items=['name'])
           .drop_duplicates()
           .values.flatten().tolist()
 )
 
 residents = (dat_tochoku
             .query('position == "レジデント"')
-            .filter(items=['new_name'])
+            .filter(items=['name'])
             .drop_duplicates()
             .values.flatten().tolist()
     )
+
+days_in_month = (dat_tochoku
+        .filter(regex=r'\d月\d{1,2}日')
+        .shape[1])
+
+holidays = [5, 6, 12, 13, 19, 20, 26, 27]
 
 #%%
 import pulp
 import random
 #%%
 # 担当者リスト
-employees = ['社員1', '社員2', '社員3', '社員4', '社員5', '社員6',
-             'アルバイト1', 'アルバイト2', 'アルバイト3', 'アルバイト4', 'アルバイト5', 'アルバイト6']
+# all_members = ['社員1', '社員2', '社員3', '社員4', '社員5', '社員6',
+#              'アルバイト1', 'アルバイト2', 'アルバイト3', 'アルバイト4', 'アルバイト5', 'アルバイト6']
 
-# 社員とアルバイトの区別
-full_timers = employees[:6]
-part_timers = employees[6:]
+# # 社員とアルバイトの区別
+# staffs = all_members[:6]
+# part_timers = all_members[6:]
 
 # 希望日と不可日をランダムに設定する関数
-def generate_availability():
-    days = list(range(7))
-    hope_days = random.sample(days, 2)
-    remaining_days = [day for day in days if day not in hope_days]
-    no_days = random.sample(remaining_days, 2)
-    return hope_days, no_days
+# def generate_availability():
+#     days = list(range(7))
+#     hope_days = random.sample(days, 2)
+#     remaining_days = [day for day in days if day not in hope_days]
+#     no_days = random.sample(remaining_days, 2)
+#     return hope_days, no_days
 
-availability = {emp: {'希望日': [], '不可日': []} for emp in employees}
-for emp in employees:
-    availability[emp]['希望日'], availability[emp]['不可日'] = generate_availability()
+# availability = {emp: {'希望日': [], '不可日': []} for emp in all_members}
+# for emp in all_members:
+#     availability[emp]['希望日'], availability[emp]['不可日'] = generate_availability()
 
 # Pulpの問題を定義
 prob = pulp.LpProblem('ShiftScheduling', pulp.LpMinimize)
 
 # 変数の定義
-x = pulp.LpVariable.dicts('shift', ((emp, day) for emp in employees for day in range(7)), cat='Binary')
+x = pulp.LpVariable.dicts('shift', ((emp, day) for emp in all_members for day in range(days_in_month)), cat='Binary')
 
 # 各担当者のシフト回数をカウントする変数
-shift_count = {emp: pulp.lpSum([x[(emp, day)] for day in range(7)]) for emp in employees}
+shift_count = {emp: pulp.lpSum([x[(emp, day)] for day in range(days_in_month)]) for emp in all_members}
 
 # 最小および最大シフト回数の変数
 min_shifts = pulp.LpVariable('min_shifts', lowBound=0, cat='Integer')
@@ -112,23 +118,23 @@ max_shifts = pulp.LpVariable('max_shifts', lowBound=0, cat='Integer')
 prob += max_shifts - min_shifts
 
 # 各曜日のシフト人数の制約
-for day in range(7):
-    if day < 5:  # 平日
-        prob += (pulp.lpSum([x[(emp, day)] for emp in employees]) == 3)
-        prob += (pulp.lpSum([x[(emp, day)] for emp in full_timers]) >= 1)
-        prob += (pulp.lpSum([x[(emp, day)] for emp in part_timers]) >= 1)
-    else:  # 土日
-        prob += (pulp.lpSum([x[(emp, day)] for emp in employees]) == 4)
-        prob += (pulp.lpSum([x[(emp, day)] for emp in full_timers]) >= 1)
-        prob += (pulp.lpSum([x[(emp, day)] for emp in part_timers]) >= 2)
+for day in range(days_in_month):
+    if day in holidays:  # holiday
+        prob += (pulp.lpSum([x[(emp, day)] for emp in all_members]) == 3)
+        prob += (pulp.lpSum([x[(emp, day)] for emp in staffs]) >= 1)
+        prob += (pulp.lpSum([x[(emp, day)] for emp in residents]) >= 1)
+    else:  # weekday
+        prob += (pulp.lpSum([x[(emp, day)] for emp in all_members]) == 4)
+        prob += (pulp.lpSum([x[(emp, day)] for emp in staffs]) >= 1)
+        prob += (pulp.lpSum([x[(emp, day)] for emp in residents]) >= 2)
 
 # 不可日の制約
-for emp in employees:
-    for day in availability[emp]['不可日']:
+for emp in all_members:
+    for day in availability_dict[emp]['不可日']:
         prob += (x[(emp, day)] == 0)
 
 # 担当者の間隔制約
-# for emp in employees:
+# for emp in all_members:
 #     for day in range(7):
 #         if day < 3:
 #             prob += pulp.lpSum(x[(emp, d)] for d in range(0, day+4)) <= 1
@@ -138,7 +144,7 @@ for emp in employees:
 #             prob += pulp.lpSum(x[(emp, d)] for d in range(day-3, 7)) <= 1
 
 # 最小および最大シフト回数の制約
-for emp in employees:
+for emp in all_members:
     prob += (shift_count[emp] >= min_shifts)
     prob += (shift_count[emp] <= max_shifts)
 
@@ -149,18 +155,18 @@ prob += (max_shifts - min_shifts <= 2)
 prob.solve()
 
 # 結果の表示
-for day in range(7):
-    assigned = [emp for emp in employees if pulp.value(x[(emp, day)]) == 1]
+for day in range(days_in_month):
+    assigned = [emp for emp in all_members if pulp.value(x[(emp, day)]) == 1]
     print(f'Day {day + 1}: {", ".join(assigned)}')
 
 # 希望日と不可日を表示
 print("\n希望日と不可日:")
-for emp in employees:
+for emp in all_members:
     print(f'{emp} - 希望日: {availability[emp]["希望日"]}, 不可日: {availability[emp]["不可日"]}')
 
 # 各担当者のシフト回数の表示
 print("\n各担当者のシフト回数:")
-for emp in employees:
+for emp in all_members:
     print(f'{emp}: {pulp.value(shift_count[emp])}回')
 
 # %%
